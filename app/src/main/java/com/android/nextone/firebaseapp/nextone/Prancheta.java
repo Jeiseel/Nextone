068e62fd10e4e942182494fcdb03949ff4db2cf4
Jeiseel Rodrigues <jeiseel.rodrigues@dce.ufpb.br>
2018-09-11 15:14:07 -0300

    Segundo Commit

--- app/src/main/java/com/android/nextone/firebaseapp/nextone/Prancheta.java
+++ app/src/main/java/com/android/nextone/firebaseapp/nextone/Prancheta.java
@@ -11,7 +11,7 @@ public class Prancheta {
     public Prancheta() {
         this.taxistas = new ArrayList<Taxista>();
     }
-
+    //Metodo de cadastra taxista em praça e verifica se o mesmo já está cadastrado por sua CNH.
     public void cadastraTaxista(Taxista taxista) throws TaxistaJaCadastradoException {
         for (Taxista t : this.taxistas) {
             if (t.getCnh().equals(taxista.getCnh())) {
@@ -20,7 +20,7 @@ public class Prancheta {
         }
         this.taxistas.add(taxista);
     }
-
+    //Remove taxista da lista pegando sua CNH.
     public void removeTaxistaDaLista(String cnh) {
         for (int k = 0; k < taxistas.size(); k++) {
             if (taxistas.get(k).equals(cnh)) {
@@ -31,7 +31,7 @@ public class Prancheta {
 
         }
     }
-
+    //pesquisa taxista pela CNH. retorna o taxista ou exceção de não existe.
     public Taxista pesquisaTaxista(String cnh) throws TaxistaNaoExisteException {
         for (Taxista t : this.taxistas) {
             if (t.getCnh().equals(cnh)) {
@@ -40,7 +40,7 @@ public class Prancheta {
         }
         throw new TaxistaNaoExisteException("Não Foi encontrado nem um taxista com essa CNH!!");
     }
-
+    //metodo que pesquisa taxista por sua placa do carro. ou retorna exceção.
     public Taxista pesquisaTaxistaPorPlacaCarro(String placaCarro) throws TaxistaNaoExisteException {
         for (Taxista y : this.taxistas) {
             if (y.getPlacaCarro().equals(placaCarro)) {
@@ -49,7 +49,7 @@ public class Prancheta {
         }
         throw new TaxistaNaoExisteException("Não Foi encontrado nem um taxista com essa CNH!!\"");
     }
-
+        //Cadastra a praça por nome e com sua quantidade de menbros.
     public void cadastraPraca(String nomeDaPraça, int maxTaxistas) throws PracaJaCadastradaException {
         Praca praca = new Praca(nomeDaPraça, maxTaxistas);
         for (Praca p : this.pracas) {
@@ -61,7 +61,7 @@ public class Prancheta {
         }
     }
 
-
+    //pesquisa praça por nome.
     public Praca pesquisaPraca(String nomeDaPraça) throws PracaNaoExisteException{
      for(Praca p: this.pracas){
          if(p.getNomeDaPraça().equals(nomeDaPraça)){
